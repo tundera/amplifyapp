5954fcdadf5c2a9d1486bd79ad2123d4cfa4b710
tundera <tunderadev@gmail.com>
2021-02-27 15:40:20 -0700

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
+
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
